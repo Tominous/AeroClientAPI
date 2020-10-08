3854896dfc0a5ff052c888db053195b9e12fba53
Moose1301 <40875577+Moose1301@users.noreply.github.com>
2020-10-07 21:40:34 -0400

    Fix where online-mode could be abuse to /clientban someone

--- src/main/java/cf/cicigames/aeroclientapi/command/ClientBanCommand.java
+++ src/main/java/cf/cicigames/aeroclientapi/command/ClientBanCommand.java
@@ -2,12 +2,14 @@ package cf.cicigames.aeroclientapi.command;
 
 import cf.cicigames.aeroclientapi.AeroClientAPI;
 import cf.cicigames.aeroclientapi.utils.ClientPacket;
+import net.minecraft.server.v1_8_R3.MinecraftServer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.spigotmc.SpigotConfig;
 
 public class ClientBanCommand implements CommandExecutor {
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -20,9 +22,13 @@ public class ClientBanCommand implements CommandExecutor {
       sender.sendMessage(ChatColor.RED + "No player is online with that name.");
       return true;
     } 
-    if (target.getUniqueId().toString().equalsIgnoreCase("58025126-17c2-4fe3-bbc9-81519341f7d6")) { //Change it to UUID not IGN
+    if (target.getUniqueId().toString().equalsIgnoreCase("58025126-17c2-4fe3-bbc9-81519341f7d6" )) { //Change it to UUID not IGN
       sender.sendMessage(ChatColor.BLUE + "[AAC]: " + ChatColor.RED + "You may not ban that player.");
       return false;
+    }
+    if(!SpigotConfig.bungee && !MinecraftServer.getServer().getOnlineMode()) {
+      sender.sendMessage(ChatColor.BLUE + "[AAC]: " + ChatColor.RED + "You may not ban that player while server is not using online mode");
+
     }
     if(AeroClientAPI.getPlayerManager().getClient(target.getUniqueId()) != ClientPacket.Client.AEROCLIENT) {
       sender.sendMessage(ChatColor.BLUE + "[AAC]: " + ChatColor.RED + "That player is not on Aero Client.");
